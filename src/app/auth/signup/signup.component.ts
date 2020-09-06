c543321ecde6615230d5eafe23242ba4022b8ee3
Marco Tedone <marco@techwings.io>
2020-09-06 12:13:54 +0100

    Add custom validators

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, Validator } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
+import { UniqueUsername } from '../validators/unique-username';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  constructor(
+    private matchValidator: MatchPassword,
+    private uniqueUsername: UniqueUsername
+  ) {}
+  authForm = new FormGroup(
+    {
+      username: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/),
+        ],
+        [this.uniqueUsername.validate]
+      ),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+      passwordConfirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+    },
+    { validators: [this.matchValidator.validate] }
+  );
+
+  ngOnInit(): void {}
+}
